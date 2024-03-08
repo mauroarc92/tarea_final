62b3c169422b1a88253055f3d8440e7b43a6f2f4
mauroarc92 <mauricioarcila53@gmail.com>
2024-03-08 17:17:06 -0500

    primer commit

---
+++ src/main/java/com/mycompany/finalp/Finalp.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.finalp;
+
+/**
+ *
+ * @author mauri
+ */
+public class Finalp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+       Pantalla vista  = new Pantalla();
+       vista.setVisible(true);
+       vista.setLocationRelativeTo(null);
+        
+    }
+    //link al video : https://youtu.be/4WO1qN5yRvg
+}
